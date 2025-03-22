e5282882c8f75bf3faca7753e217d118825b03a6
Tobias Ruano <tobias@mobilenik.com>
2025-03-22 12:02:01 -0300

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Tobias Ruano on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeStoreTests/FakeStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  FakeStoreTests.swift
+//  FakeStoreTests
+//
+//  Created by Tobias Ruano on 22/03/2025.
+//
+
+import Testing
+
+struct FakeStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
